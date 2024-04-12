1c20d5908cc1bd6a1da52f452e61c0e5d89a76e9
NehuenLlanosItba <nllanos@itba.edu.ar>
2024-04-12 11:48:00 -0300

    Main and creation of HeavyMovingParticle. Update Particle class in TP1.
    Co-authored-by: AlitoElMansito <alejo@misterflores.com>

---
+++ src/main/java/ar/edu/itba/ss/tp3/HeavyMovingParticle.java
@@ -0,0 +1,108 @@
+package ar.edu.itba.ss.tp3;
+
+import ar.edu.itba.ss.tp2.MovingParticle;
+
+public class HeavyMovingParticle extends MovingParticle {
+    private double mass;
+
+    protected HeavyMovingParticle(String identifier, double radius, double x, double y, double velocity, double angle, double mass) {
+        super(identifier, radius, x, y, velocity, angle);
+        this.mass = mass;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        HeavyMovingParticle that = (HeavyMovingParticle) o;
+
+        return Double.compare(mass, that.mass) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(mass);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public static class Builder {
+        private String identifier;
+        private Double radius;
+        private Double x;
+        private Double y;
+        private Double velocity;
+        private Double angle;
+        private Double mass;
+
+        protected Builder() {
+
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder withIdentifier(String identifier) {
+            this.identifier = identifier;
+            return this;
+        }
+
+        public Builder withRadius(double radius) {
+            this.radius = radius;
+            return this;
+        }
+
+        public Builder withX(double x) {
+            this.x = x;
+            return this;
+        }
+
+        public Builder withY(double y) {
+            this.y = y;
+            return this;
+        }
+
+        public Builder withVelocity(double velocity) {
+            this.velocity = velocity;
+            return this;
+        }
+
+        public Builder withAngle(double angle) {
+            this.angle = angle;
+            return this;
+        }
+
+        public Builder withMass(double mass) {
+            this.mass = mass;
+            return this;
+        }
+
+        public HeavyMovingParticle build() {
+            if (this.identifier == null || this.radius == null || this.x == null || this.y == null || this.velocity == null || this.angle == null || this.mass == null) {
+                throw new IllegalStateException();
+            }
+            return new HeavyMovingParticle(
+                    this.identifier,
+                    this.radius,
+                    this.x,
+                    this.y,
+                    this.velocity,
+                    this.angle,
+                    this.mass
+            );
+        }
+    }
+}
--- src/main/java/ar/edu/itba/ss/tp3/Main.java
+++ src/main/java/ar/edu/itba/ss/tp3/Main.java
@@ -1,7 +1,85 @@
 package ar.edu.itba.ss.tp3;
 
+import ar.edu.itba.ss.cim.CellIndexMethod;
+import ar.edu.itba.ss.cim.Plane;
+import ar.edu.itba.ss.tp2.MovingParticle;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
+    private static double getRandomAngle() {
+        return 2 * Math.PI * Math.random() - Math.PI;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> data = null;
+        try (Stream<String> stream = Files.lines(Paths.get("input.txt"))) {
+            data = stream.toList();
+        } catch (Exception e) {
+            System.err.println("No input file found");
+            System.exit(1);
+        }
+
+        if (data.size() != 11) {
+            throw new IllegalStateException();
+        }
+
+        final int particleCount = Integer.parseInt(data.get(0)); // N
+        final double planeLength = Double.parseDouble(data.get(1)); // L
+        final double interactionRadius = Double.parseDouble(data.get(2)); // r_c
+        final int eventCount = Integer.parseInt(data.get(3)); // N
+        final double particleRadius = Double.parseDouble(data.get(4)); // r_particles
+        final double particleMass = Double.parseDouble(data.get(5)); // m_particles
+        final double particleVelocity = Double.parseDouble(data.get(6)); //v_0_particles
+        final double obstacleRadius = Double.parseDouble(data.get(7)); // r_obstacle
+        final double obstacleMass = Double.parseDouble(data.get(8)); // m_obstacle
+        final double obstacleVelocity = Double.parseDouble(data.get(9)); // v_0_obstacle
+        final boolean obstacleMoves = Boolean.parseBoolean(data.get(10)); // obstacle_moves
+
+        Plane.Builder<HeavyMovingParticle> planeBuilder = Plane.Builder.newBuilder();
+
+        HeavyMovingParticle obstacle = HeavyMovingParticle.Builder.newBuilder()
+                .withIdentifier("obstacle")
+                .withX(planeLength / 2)
+                .withY(planeLength / 2)
+                .withRadius(obstacleRadius)
+                .withMass(obstacleMass)
+                .withVelocity(obstacleVelocity)
+                .withAngle(getRandomAngle())
+                .build();
+
+        List<HeavyMovingParticle> particles = new ArrayList<>();
+        particles.add(obstacle);
+        for (int i = 0; i < particleCount; i++) {
+            HeavyMovingParticle particle = HeavyMovingParticle.Builder.newBuilder()
+                    .withIdentifier(String.format("p_%d", i))
+                    .withX(0)
+                    .withY(0)
+                    .withRadius(particleRadius)
+                    .withMass(particleMass)
+                    .withVelocity(particleVelocity)
+                    .withAngle(getRandomAngle())
+                    .build();
+
+            do {
+                particle.setX(Math.random() * (planeLength - 2 * particleRadius) + particleRadius);
+                particle.setY(Math.random() * (planeLength - 2 * particleRadius) + particleRadius);
+            } while (particles.stream().anyMatch(p -> p.distanceTo(particle, true) < 0));
+
+            particles.add(particle);
+        }
+        planeBuilder = planeBuilder.withParticles(particles);
+
+        CellIndexMethod.Builder<HeavyMovingParticle> cimBuilder = CellIndexMethod.Builder.newBuilder();
+        final CellIndexMethod<HeavyMovingParticle> cim = cimBuilder
+                .withInteractionRadius(interactionRadius)
+                .withPlane(planeBuilder.withLength(planeLength).build())
+                .build();
+
+        
     }
 }
